afd67f2bfad1e5f3078e07dad56c08106d9cd0db
Nathan GUIGUI <nathan.guigui13390@gmail.com>
2022-07-12 17:08:31 +0200

    :sparkles: make it work on ios

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import {WebView} from 'react-native-webview';
 import {request, PERMISSIONS} from 'react-native-permissions';
-import {Platform, Text, View} from 'react-native';
+import {Platform, SafeAreaView, Text, View} from 'react-native';
 
 export default function App() {
   const [isGranted, setIsGranted] = useState(false);
@@ -27,20 +27,22 @@ export default function App() {
   };
 
   return (
-    <View style={{flex: 1, width: '100%', height: '100%'}}>
-      {isGranted ? (
-        <WebView
-          style={{flex: 1, width: '100%', height: '100%'}}
-          useWebKit
-          originWhitelist={['*']}
-          allowsInlineMediaPlayback
-          source={{
-            uri: 'https://verify.synaps.io?session_id=aaaaaa-bbbbbb-cccccc-dddddd',
-          }}
-        />
-      ) : (
-        <Text onPress={checkPermissions}>Accorder les permissions</Text>
-      )}
-    </View>
+    <SafeAreaView style={{flex: 1}}>
+      <View style={{flex: 1, width: '100%', height: '100%'}}>
+        {isGranted ? (
+          <WebView
+            style={{flex: 1, width: '100%', height: '100%'}}
+            useWebKit
+            originWhitelist={['*']}
+            allowsInlineMediaPlayback
+            source={{
+              uri: 'https://verify.synaps.io?session_id=aaaaaa-bbbbbb-cccccc-dddddd',
+            }}
+          />
+        ) : (
+          <Text onPress={checkPermissions}>Accorder les permissions</Text>
+        )}
+      </View>
+    </SafeAreaView>
   );
 }
