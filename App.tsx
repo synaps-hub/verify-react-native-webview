c88741d93806b7b468157137f9b7b64b2308448b
nathan guigui <nathan.guigui13390@gmail.com>
2022-07-12 16:28:58 +0200

    :tada: add base example

---
+++ App.tsx
@@ -0,0 +1,46 @@
+import React, {useEffect, useState} from 'react';
+import {WebView} from 'react-native-webview';
+import {request, PERMISSIONS} from 'react-native-permissions';
+import {Platform, Text, View} from 'react-native';
+
+export default function App() {
+  const [isGranted, setIsGranted] = useState(false);
+
+  useEffect(() => {
+    checkPermissions();
+  }, []);
+
+  const checkPermissions = async () => {
+    try {
+      if (Platform.OS === 'android') {
+        await request(PERMISSIONS.ANDROID.CAMERA);
+        await request(PERMISSIONS.ANDROID.RECORD_AUDIO);
+        setIsGranted(true);
+      } else {
+        await request(PERMISSIONS.IOS.CAMERA);
+        await request(PERMISSIONS.IOS.MICROPHONE);
+        setIsGranted(true);
+      }
+    } catch (e) {
+      setIsGranted(false);
+    }
+  };
+
+  return (
+    <View style={{flex: 1, width: '100%', height: '100%'}}>
+      {isGranted ? (
+        <WebView
+          style={{flex: 1, width: '100%', height: '100%'}}
+          useWebKit
+          originWhitelist={['*']}
+          allowsInlineMediaPlayback
+          source={{
+            uri: 'https://verify.synaps.io?session_id=aaaaaa-bbbbbb-cccccc-dddddd',
+          }}
+        />
+      ) : (
+        <Text onPress={checkPermissions}>Accorder les permissions</Text>
+      )}
+    </View>
+  );
+}
